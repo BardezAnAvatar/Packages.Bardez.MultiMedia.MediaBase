09f4787243208dddb8ea5d00431bd68ffff1b9f4
Erik "Bardez" Frantz <bardez@bardez.net>
2012-01-21 07:04:10 +0000

    Reverted back to bilinear resize
    git-tfs-id: [http://bardezserver2:8080/tfs/DefaultCollection]$/Infinity Engine;C164

--- Image/Resize/Nearest Neighbor Integer Space.cs
+++ Image/Resize/Nearest Neighbor Integer Space.cs
@@ -15,19 +15,20 @@ namespace Bardez.Projects.InfinityPlus1.FileFormats.External.Image.Resize
         /// <param name="targetHeight">Output target height</param>
         /// <param name="targetWidth">Output target width</param>
         /// <returns>An Array of integer values for later transforms</returns>
+        /// <remarks>Occasionally maximizes the Green resultant component, but only sometimes. Not remarkably faster than Bilinear.</remarks>
         public static Int32[] NearestNeighborResampleInteger(Int32[] data, Int32 actualHeight, Int32 actualWidth, Int32 dataHeight, Int32 dataWidth, Int32 targetHeight, Int32 targetWidth)
         {
             Int32[] destinationData = new Int32[targetWidth * targetHeight];
 
             for (Int32 v = 0; v < (actualHeight * targetHeight); v += targetHeight)
             {
-                Int32 yDst = (v / targetHeight) * targetWidth;
-                Int32 ySrc = (v / actualHeight) * dataWidth;
+                Int32 yDst = ((v / actualHeight) * targetWidth);
+                Int32 ySrc = ((v / targetHeight)  * dataWidth);
 
                 for (Int32 h = 0; h < (actualWidth * targetWidth); h += targetWidth)
                 {
-                    Int32 xDst = (h / targetWidth);
-                    Int32 xDest = h / actualWidth;
+                    Int32 xDst = (h / actualWidth);
+                    Int32 xDest = (h / targetWidth);
                     destinationData[yDst + xDst] = data[ySrc + xDest];
                 }
             }
