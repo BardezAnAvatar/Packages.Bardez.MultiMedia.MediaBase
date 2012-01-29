60290d8869a51935ae88b4695cb2167bc5632f10
Erik "Bardez" Frantz <bardez@bardez.net>
2012-01-29 17:03:36 +0000

    Added in interface for image types with multiple logical images
    git-tfs-id: [http://bardezserver2:8080/tfs/DefaultCollection]$/Infinity Engine;C199

---
+++ MediaBase/Video/IImageSet.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace Bardez.Projects.InfinityPlus1.FileFormats.MediaBase.Video
+{
+    /// <summary>Interface to getting a specific image from a format that logically stores multiple frames</summary>
+    public interface IImageSet
+    {
+        /// <summary>Gets a frame image from the pixel data already in place</summary>
+        /// <param name="index">Index of the frame to get</param>
+        /// <returns>A frame containing the pixel data</returns>
+        Frame GetFrame(Int32 index);
+    }
+}
