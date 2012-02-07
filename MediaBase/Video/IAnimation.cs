afc1bacd396b4c3fa49543072a1306af70996d1c
Erik "Bardez" Frantz <bardez@bardez.net>
2012-02-07 00:09:12 +0000

    Populated the Interface
    git-tfs-id: [http://bardezserver2:8080/tfs/DefaultCollection]$/Infinity Engine;C228

--- MediaBase/Video/IAnimation.cs
+++ MediaBase/Video/IAnimation.cs
@@ -5,7 +5,11 @@ using System.Text;
 
 namespace Bardez.Projects.InfinityPlus1.FileFormats.MediaBase.Video
 {
-    public interface IAnimation
+    /// <summary>Interface to pull information indicating animation set information</summary>
+    public interface IAnimation : IImageSet
     {
+        /// <summary>Returns an IList containing an IList of indeces meant to be used in conjunction with <see cref="IImageSet.GetFrame(Int32)"/></summary>
+        /// <returns></returns>
+        IList<IList<Int32>> GetFrameAnimations();
     }
 }
