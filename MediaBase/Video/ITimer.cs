8e864bba0c9ade0d276c93075b8a3cb977b2dbb7
Erik "Bardez" Frantz <bardez@bardez.net>
2012-03-27 00:36:50 +0000

    Added in an ITimer interface
    git-tfs-id: [http://bardezserver2:8080/tfs/DefaultCollection]$/Infinity Engine;C297

---
+++ MediaBase/Video/ITimer.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace Bardez.Projects.InfinityPlus1.FileFormats.MediaBase.Video
+{
+    /// <summary>Interface for a custom timer</summary>
+    public interface ITimer : IDisposable
+    {
+        /// <summary>Frequency period of the timer interval</summary>
+        UInt32 Interval { get; set; }
+
+        /// <summary>Represents when the timer was last started (stop sets to <see cref="TimeSpan.MinValue"/>)</summary>
+        TimeSpan StartTime { get; set; }
+
+        /// <summary>Public-facing timer event</summary>
+        event Action<TimeSpan> Elapsed;
+
+        /// <summary>Starts the timer</summary>
+        void Start();
+
+        /// <summary>Stops the timer</summary>
+        void Stop();
+
+        /// <summary>Resets the time for when the timer goes off</summary>
+        void ResetStartTime();
+    }
+}
