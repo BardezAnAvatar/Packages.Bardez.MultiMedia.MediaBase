c7841052d79836dfd15389c9a06f8472f072e9eb
Erik "Bardez" Frantz <bardez@bardez.net>
2012-07-10 07:15:20 +0000

    Added a YUV pixel format and a method to convert back to LibAVPixelFormat
    git-tfs-id: [http://bardezserver2:8080/tfs/DefaultCollection]$/Infinity Engine;C418

--- MediaBase/Video/Pixels/Enums/PixelFormat.cs
+++ MediaBase/Video/Pixels/Enums/PixelFormat.cs
@@ -6,6 +6,9 @@ namespace Bardez.Projects.InfinityPlus1.FileFormats.MediaBase.Video.Pixels.Enums
     /// <summary>Represents a number of pixel formats that the binary data represents</summary>
     public enum PixelFormat
     {
+        /// <summary>An invalid pixel format type</summary>
+        Invalid,
+
         /// <summary>32-bit RGB with an alpha channel. Represented in R, G, B, A order.</summary>
         RGBA_R8G8B8A8,
 
@@ -26,6 +29,9 @@ namespace Bardez.Projects.InfinityPlus1.FileFormats.MediaBase.Video.Pixels.Enums
 
         /// <summary>Represents the YCbCr colorspace pixel format used by JFIF (it specifies slightly different color conversions than other sources). Represented in Y, Cb, Cr order.</summary>
         YCbCrJpeg,
+
+        /// <summary>Represents YUV colorspace, with 4 Y pixels (square) to every one Cb and Cr pixel</summary>
+        YUV_YUV_Y4Cb1Cr1,
     }
 
     /// <summary>Extension class for converting the PixelFormat enum to other destination pixel format enumerators</summary>
@@ -57,6 +63,44 @@ namespace Bardez.Projects.InfinityPlus1.FileFormats.MediaBase.Video.Pixels.Enums
                     break;
                 case PixelFormat.RGBA_R8G8B8A8:
                     format = LibAVPixelFormat.PIX_FMT_RGBA;
+                    break;
+                case PixelFormat.YUV_YUV_Y4Cb1Cr1:
+                    format = LibAVPixelFormat.PIX_FMT_YUV420P;
+                    break;
+            }
+
+            return format;
+        }
+
+        /// <summary>Converts the LibAV Pixel Format instance to a PixelFormat enumerator</summary>
+        /// <param name="source">Source LibAVPixelFormat to translate</param>
+        /// <returns>The translated pixel format, or LibAVPixelFormat.PIX_FMT_NONE if no translation available.</returns>
+        public static PixelFormat FromLibAVPixelFormat(LibAVPixelFormat source)
+        {
+            PixelFormat format = PixelFormat.Invalid;
+
+            switch (source)
+            {
+                case LibAVPixelFormat.PIX_FMT_BGR555LE:
+                    format = PixelFormat.RGB_B5G5R5X1;
+                    break;
+                case LibAVPixelFormat.PIX_FMT_BGR24:
+                    format = PixelFormat.RGB_B8G8R8;
+                    break;
+                case LibAVPixelFormat.PIX_FMT_RGB555LE:
+                    format = PixelFormat.RGB_R5G5B5X1;
+                    break;
+                case LibAVPixelFormat.PIX_FMT_RGB24:
+                    format = PixelFormat.RGB_R8G8B8;
+                    break;
+                case LibAVPixelFormat.PIX_FMT_BGRA:
+                    format = PixelFormat.RGBA_B8G8R8A8;
+                    break;
+                case LibAVPixelFormat.PIX_FMT_RGBA:
+                    format = PixelFormat.RGBA_R8G8B8A8;
+                    break;
+                case LibAVPixelFormat.PIX_FMT_YUV420P:
+                    format = PixelFormat.YUV_YUV_Y4Cb1Cr1;
                     break;
             }
 
