6b64570109b2f1f31f1801bde2a824bb62a88a0f
Erik "Bardez" Frantz <bardez@bardez.net>
2012-07-10 02:32:03 +0000

    Added in extension methods to convert the enumerator to LibAV Pixel Formats
    git-tfs-id: [http://bardezserver2:8080/tfs/DefaultCollection]$/Infinity Engine;C402

--- MediaBase/Video/Pixels/Enums/PixelFormat.cs
+++ MediaBase/Video/Pixels/Enums/PixelFormat.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using Bardez.Projects.MultiMedia.LibAV;
 
 namespace Bardez.Projects.InfinityPlus1.FileFormats.MediaBase.Video.Pixels.Enums
 {
@@ -25,5 +26,41 @@ namespace Bardez.Projects.InfinityPlus1.FileFormats.MediaBase.Video.Pixels.Enums
 
         /// <summary>Represents the YCbCr colorspace pixel format used by JFIF (it specifies slightly different color conversions than other sources). Represented in Y, Cb, Cr order.</summary>
         YCbCrJpeg,
+    }
+
+    /// <summary>Extension class for converting the PixelFormat enum to other destination pixel format enumerators</summary>
+    public static class PixelFormatExtender
+    {
+        /// <summary>Converts the PixelFormat instance to a LibAV Pixel Format enumerator</summary>
+        /// <param name="source">Source PixelFormat to translate</param>
+        /// <returns>The translated pixel format, or LibAVPixelFormat.PIX_FMT_NONE if no translation available.</returns>
+        public static LibAVPixelFormat ToLibAVPixelFormat(this PixelFormat source)
+        {
+            LibAVPixelFormat format = LibAVPixelFormat.PIX_FMT_NONE;
+
+            switch (source)
+            {
+                case PixelFormat.RGB_B5G5R5X1:
+                    format = LibAVPixelFormat.PIX_FMT_BGR555LE;
+                    break;
+                case PixelFormat.RGB_B8G8R8:
+                    format = LibAVPixelFormat.PIX_FMT_BGR24;
+                    break;
+                case PixelFormat.RGB_R5G5B5X1:
+                    format = LibAVPixelFormat.PIX_FMT_RGB555LE;
+                    break;
+                case PixelFormat.RGB_R8G8B8:
+                    format = LibAVPixelFormat.PIX_FMT_RGB24;
+                    break;
+                case PixelFormat.RGBA_B8G8R8A8:
+                    format = LibAVPixelFormat.PIX_FMT_BGRA;
+                    break;
+                case PixelFormat.RGBA_R8G8B8A8:
+                    format = LibAVPixelFormat.PIX_FMT_RGBA;
+                    break;
+            }
+
+            return format;
+        }
     }
 }
