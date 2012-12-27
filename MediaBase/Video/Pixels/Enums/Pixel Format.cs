98e8fb4cee88b47a893fae80a2aaa32cb96e07f0
Erik "Bardez" Frantz <bardez@bardez.net>
2012-12-27 08:15:46 +0000

    Removed PixelFormatExtender and branched it into LibAV Wrapper project
    git-tfs-id: [http://bardezserver2:8080/tfs/DefaultCollection]$/Infinity Engine;C537

--- MediaBase/Video/Pixels/Enums/Pixel Format.cs
+++ MediaBase/Video/Pixels/Enums/Pixel Format.cs
@@ -1,5 +1,4 @@
 ﻿using System;
-using Bardez.Projects.MultiMedia.LibAV;
 
 namespace Bardez.Projects.InfinityPlus1.FileFormats.MediaBase.Video.Pixels.Enums
 {
@@ -35,85 +34,5 @@ namespace Bardez.Projects.InfinityPlus1.FileFormats.MediaBase.Video.Pixels.Enums
 
         /// <summary>Represents RBG32 with a palette</summary>
         RGB_32_Paletted,
-    }
-
-    /// <summary>Extension class for converting the PixelFormat enum to other destination pixel format enumerators</summary>
-    public static class PixelFormatExtender
-    {
-        /// <summary>Converts the PixelFormat instance to a LibAV Pixel Format enumerator</summary>
-        /// <param name="source">Source PixelFormat to translate</param>
-        /// <returns>The translated pixel format, or LibAVPixelFormat.PIX_FMT_NONE if no translation available.</returns>
-        public static LibAVPixelFormat ToLibAVPixelFormat(this PixelFormat source)
-        {
-            LibAVPixelFormat format = LibAVPixelFormat.PIX_FMT_NONE;
-
-            switch (source)
-            {
-                case PixelFormat.RGB_B5G5R5X1:
-                    format = LibAVPixelFormat.PIX_FMT_BGR555LE;
-                    break;
-                case PixelFormat.RGB_B8G8R8:
-                    format = LibAVPixelFormat.PIX_FMT_BGR24;
-                    break;
-                case PixelFormat.RGB_R5G5B5X1:
-                    format = LibAVPixelFormat.PIX_FMT_RGB555LE;
-                    break;
-                case PixelFormat.RGB_R8G8B8:
-                    format = LibAVPixelFormat.PIX_FMT_RGB24;
-                    break;
-                case PixelFormat.RGBA_B8G8R8A8:
-                    format = LibAVPixelFormat.PIX_FMT_BGRA;
-                    break;
-                case PixelFormat.RGBA_R8G8B8A8:
-                    format = LibAVPixelFormat.PIX_FMT_RGBA;
-                    break;
-                case PixelFormat.YUV_YUV_Y4Cb1Cr1:
-                    format = LibAVPixelFormat.PIX_FMT_YUV420P;
-                    break;
-                case PixelFormat.RGB_32_Paletted:
-                    format = LibAVPixelFormat.PIX_FMT_PAL8;
-                    break;
-            }
-
-            return format;
-        }
-
-        /// <summary>Converts the LibAV Pixel Format instance to a PixelFormat enumerator</summary>
-        /// <param name="source">Source LibAVPixelFormat to translate</param>
-        /// <returns>The translated pixel format, or LibAVPixelFormat.PIX_FMT_NONE if no translation available.</returns>
-        public static PixelFormat FromLibAVPixelFormat(LibAVPixelFormat source)
-        {
-            PixelFormat format = PixelFormat.Invalid;
-
-            switch (source)
-            {
-                case LibAVPixelFormat.PIX_FMT_BGR555LE:
-                    format = PixelFormat.RGB_B5G5R5X1;
-                    break;
-                case LibAVPixelFormat.PIX_FMT_BGR24:
-                    format = PixelFormat.RGB_B8G8R8;
-                    break;
-                case LibAVPixelFormat.PIX_FMT_RGB555LE:
-                    format = PixelFormat.RGB_R5G5B5X1;
-                    break;
-                case LibAVPixelFormat.PIX_FMT_RGB24:
-                    format = PixelFormat.RGB_R8G8B8;
-                    break;
-                case LibAVPixelFormat.PIX_FMT_BGRA:
-                    format = PixelFormat.RGBA_B8G8R8A8;
-                    break;
-                case LibAVPixelFormat.PIX_FMT_RGBA:
-                    format = PixelFormat.RGBA_R8G8B8A8;
-                    break;
-                case LibAVPixelFormat.PIX_FMT_YUV420P:
-                    format = PixelFormat.YUV_YUV_Y4Cb1Cr1;
-                    break;
-                case LibAVPixelFormat.PIX_FMT_PAL8:
-                    format = PixelFormat.RGB_32_Paletted;
-                    break;
-            }
-
-            return format;
-        }
     }
 }
