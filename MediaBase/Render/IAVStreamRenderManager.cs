790b45783c52cd9257bdd3e623bbc751108f0ec7
Erik "Bardez" Frantz <bardez@bardez.net>
2012-12-28 19:13:26 +0000

    Changed name, namespace, references and added comments
    git-tfs-id: [http://bardezserver2:8080/tfs/DefaultCollection]$/Infinity Engine;C566

--- MediaBase/Render/IAVStreamRenderManager.cs
+++ MediaBase/Render/IAVStreamRenderManager.cs
@@ -1,12 +1,10 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
 
-namespace Bardez.Projects.InfinityPlus1.FileFormats.External.Multimedia
+namespace Bardez.Projects.MultiMedia.MediaBase.Render
 {
-    /// <summary>This interface defines methods and events common to LibAV stream renderers</summary>
-    public interface IStreamRenderManager
+    /// <summary>This interface defines methods and events common to multimedia stream renderers</summary>
+    /// <remarks>Classes implementing this interface will have frame buffers to render from</remarks>
+    public interface IAVStreamRenderManager
     {
         /// <summary>
         ///     Attempts to render audio if the buffer is processed and the timespan is within the
