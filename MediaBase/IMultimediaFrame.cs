f82c62abb2080559731e131bbde954cef259782f
Erik "Bardez" Frantz <bardez@bardez.net>
2012-12-28 18:49:44 +0000

    Added in basic interface for a multimedia "frame"
    git-tfs-id: [http://bardezserver2:8080/tfs/DefaultCollection]$/Infinity Engine;C547

---
+++ MediaBase/IMultimediaFrame.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.IO;
+
+namespace Bardez.Projects.MultiMedia.MediaBase
+{
+    /// <summary>Defines an basic interface for common elements of audio, video and metadata frames of data samples</summary>
+    /// <remarks>
+    ///     Common elements for all frames are:
+    ///         data:           a Byte array, or perhaps a stream
+    ///         metadata:       something indicating parameters of the data described such as:
+    ///             data format:    something indicating the type of data represented
+    ///             audio details/height, width, sample bit depth, packing, rendering origin, etc.
+    ///         metadata has no real base that I can think of other than data format, which may not make the best
+    ///             enumeration to join together (such as LibAV). It is, however, the only common element.
+    ///     For multimedia playback, a start time and a lifetime are relevant. However, for still images, it is not.
+    ///         An animated GIF would have a lifetime, whereas a JPEG just wouldn't.
+    /// </remarks>
+    public interface IMultimediaFrame : IDisposable
+    {
+        #region Properties
+        /// <summary>Exposes the data of a multimedia frame</summary>
+        Stream Data { get; }
+        #endregion
+    }
+}
