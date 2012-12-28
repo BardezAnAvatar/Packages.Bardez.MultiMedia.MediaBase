65939e2f20f94f4dc32aa4cea8c08a5d9ba73b8e
Erik "Bardez" Frantz <bardez@bardez.net>
2012-12-28 19:12:35 +0000

    Moved to MediaBase as is appropriate
    git-tfs-id: [http://bardezserver2:8080/tfs/DefaultCollection]$/Infinity Engine;C565

---
+++ MediaBase/Render/IAVStreamRenderManager.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Bardez.Projects.InfinityPlus1.FileFormats.External.Multimedia
+{
+    /// <summary>This interface defines methods and events common to LibAV stream renderers</summary>
+    public interface IStreamRenderManager
+    {
+        /// <summary>
+        ///     Attempts to render audio if the buffer is processed and the timespan is within the
+        ///     rendering threshold
+        /// </summary>
+        /// <param name="time">Time code to render by</param>
+        void AttemptRender(TimeSpan time);
+    }
+}
