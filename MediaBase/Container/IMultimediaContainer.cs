02d4de172066beb6413923f3a20ae77cf522aad4
Erik "Bardez" Frantz <bardez@bardez.net>
2013-01-03 02:10:36 +0000

    Added in additional members to expose necessary information that the container should provide
    git-tfs-id: [http://bardezserver2:8080/tfs/DefaultCollection]$/Infinity Engine;C618

--- MediaBase/Container/IMultimediaContainer.cs
+++ MediaBase/Container/IMultimediaContainer.cs
@@ -16,6 +16,18 @@ namespace Bardez.Projects.Multimedia.MediaBase.Container
         /// <summary>Collection of Stream processing information that is both informational and controllable</summary>
         /// <remarks>This should be referenced to indicate which streams to add to buffers and which ones to ignore</remarks>
         Dictionary<Int32, StreamProcessingInfo> StreamInfo { get; }
+
+        /// <summary>Exposes the implementation-defined default audio stream found in the data</summary>
+        Int32 DefaultStreamAudio { get; }
+
+        /// <summary>Exposes the implementation-defined default video stream found in the data</summary>
+        Int32 DefaultStreamVideo { get; }
+
+        /// <summary>Exposes the implementation-defined default subtitle stream found in the data</summary>
+        Int32 DefaultStreamSubtitle { get; }
+
+        /// <summary>The frame buffer container object for the streams within this container</summary>
+        IMultimediaFrameStreamBuffers buffers { get; }
         #endregion
 
 
@@ -29,7 +41,12 @@ namespace Bardez.Projects.Multimedia.MediaBase.Container
         void OpenMedia(Stream source);
 
         /// <summary>Method to launch in a new thread which decodes the various streams and fills them into buffers</summary>
-        void DecodeStreamsThread(IMultimediaFrameStreamBuffers streams);
+        void DecodeStreamsThread();
+
+        /// <summary>Seeks the various streams to the timeset indicated</summary>
+        /// <param name="time">Time code at which the streams should start or pick up</param>
+        /// <remarks>Clear the buffers and then start filling them again</remarks>
+        void Seek(TimeSpan time);
         #endregion
     }
 }
