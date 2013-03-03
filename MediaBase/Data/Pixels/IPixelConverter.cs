eaac12bde2b2eae4dc16ffc2739383681b43b7cc
Erik "Bardez" Frantz <bardez@bardez.net>
2013-03-03 21:59:27 +0000

    Added FlipVertical and AdjustForPacking methods to the IPixelConverter interface
    git-tfs-id: [http://bardezserver2:8080/tfs/DefaultCollection]$/Infinity Engine;C685

--- MediaBase/Data/Pixels/IPixelConverter.cs
+++ MediaBase/Data/Pixels/IPixelConverter.cs
@@ -2,6 +2,7 @@
 using System.IO;
 
 using Bardez.Projects.Multimedia.MediaBase.Data.Pixels.Enums;
+using Bardez.Projects.Multimedia.MediaBase.Frame.Image;
 
 namespace Bardez.Projects.Multimedia.MediaBase.Data.Pixels
 {
@@ -20,5 +21,20 @@ namespace Bardez.Projects.Multimedia.MediaBase.Data.Pixels
         /// <param name="decodedBitDepth">The bits per pixel once decoded</param>
         /// <returns>New byte data</returns>
         MemoryStream ConvertData(MemoryStream data, PixelFormat sourceFormat, PixelFormat destinationFormat, Int32 horizontalPacking, Int32 verticalPacking, Int32 sourceWidth, Int32 sourceHeight, Int32 decodedBitDepth);
+
+        /// <summary>Flips pixel data scan line by scan line</summary>
+        /// <param name="data">Decompressed data to flip</param>
+        /// <param name="metadata">Image metadata for the source frame</param>
+        /// <returns>The vertically flipped data</returns>
+        /// <remarks>Uses the current pixel data's packing, not the destination's</remarks>
+        MemoryStream FlipVertical(MemoryStream data, ImageMetadata metadata);
+
+        /// <summary>Adjusts the packing bytes of the data</summary>
+        /// <param name="data">Data to pack</param>
+        /// <param name="metadata">Image metadata for the source frame</param>
+        /// <param name="destPackingHorizontal">target horizontal packing</param>
+        /// <param name="destPackingVertical">target vertical packing</param>
+        /// <returns>The adjusted packed bytes</returns>
+        MemoryStream AdjustForPacking(MemoryStream data, ImageMetadata metadata, Int32 destPackingHorizontal, Int32 destPackingVertical);
     }
 }
