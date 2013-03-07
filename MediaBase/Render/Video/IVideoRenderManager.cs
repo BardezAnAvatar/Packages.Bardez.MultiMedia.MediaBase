8070c775b78614d7af6a5d7b9b673b0b02a19cb1
Erik "Bardez" Frantz <bardez@bardez.net>
2013-03-07 03:01:48 +0000

    Added a summary to the render manager
    git-tfs-id: [http://bardezserver2:8080/tfs/DefaultCollection]$/Infinity Engine;C693

--- MediaBase/Render/Video/IVideoRenderManager.cs
+++ MediaBase/Render/Video/IVideoRenderManager.cs
@@ -4,6 +4,7 @@ using Bardez.Projects.Multimedia.MediaBase.Frame.Image;
 
 namespace Bardez.Projects.Multimedia.MediaBase.Render.Video
 {
+    /// <summary>Interface for a manager that renders video frames</summary>
     public interface IVideoRenderManager : IAVStreamRenderManager
     {
         /// <summary>Event to raise for rendering video output.</summary>
