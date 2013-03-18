e72e11986744451898a01eb624b64ddb092c7cdc
Erik "Bardez" Frantz <bardez@bardez.net>
2013-03-18 03:39:26 +0000

    Made all properties public (whoops!)
    git-tfs-id: [http://bardezserver2:8080/tfs/DefaultCollection]$/Infinity Engine;C713

--- MediaBase/Render/Audio/Perspective.cs
+++ MediaBase/Render/Audio/Perspective.cs
@@ -10,20 +10,20 @@ namespace Bardez.Projects.Multimedia.MediaBase.Render.Audio
     {
         #region Fields
         /// <summary>Forward orientation vector</summary>
-        Vector<Single> OrientationFront { get; set; }
+        public Vector<Single> OrientationFront { get; set; }
 
         /// <summary>Vertical orientation vector</summary>
         /// <remarks>I do not see how this isn't covered by a single vector</remarks>
-        Vector<Single> OrientationVertical { get; set; }
+        public Vector<Single> OrientationVertical { get; set; }
 
         /// <summary>Position of the perspective</summary>
-        Vector<Single> Position { get; set; }
+        public Vector<Single> Position { get; set; }
 
         /// <summary>Velocity of the perspective</summary>
-        Vector<Single> Velocity { get; set; }
+        public Vector<Single> Velocity { get; set; }
 
         /// <summary>Cone of the major area of effect (emission or receipt)</summary>
-        AudioCone Cone { get; set; }
+        public AudioCone Cone { get; set; }
         #endregion
     }
 }
